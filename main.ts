38ef4afc91b55affd922c90a55722cee1d6b25bd
MWE78710 <159971978+MWE78710@users.noreply.github.com>
2024-02-14 10:45:09 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,27 @@
-let rando = randint(-20, 20)
-if (rando >= 0 && rando < 10 || rando >= 15 && rando < 18) {
-    basic.showNumber(rando)
-}
+let Max_Bright = 0
+let B_Level = 0
+basic.forever(function () {
+    if (B_Level < 255 && Max_Bright == 0) {
+        B_Level = B_Level + 1
+        basic.pause(1)
+    }
+    if (B_Level == 255 && Max_Bright == 0) {
+        Max_Bright = 1
+    }
+    if (B_Level > 0 && Max_Bright == 1) {
+        B_Level = B_Level - 1
+        basic.showNumber(B_Level)
+        basic.pause(10)
+    }
+    if (B_Level == 0 && Max_Bright == 1) {
+        Max_Bright = 0
+    }
+    led.setBrightness(B_Level)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+})
