5fa068144dbd1f2b7eaea7a5dbcce93e29794100
MWE78710 <159971978+MWE78710@users.noreply.github.com>
2024-02-14 13:04:07 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,18 +2,18 @@ let Max_Bright = 0
 let B_Level = 0
 basic.forever(function () {
     if (B_Level < 255 && Max_Bright == 0) {
-        B_Level = B_Level + 1
-        basic.pause(1)
+        B_Level = B_Level + 10
+        basic.pause(10)
     }
-    if (B_Level == 255 && Max_Bright == 0) {
+    if (B_Level >= 255 && Max_Bright == 0) {
         Max_Bright = 1
     }
     if (B_Level > 0 && Max_Bright == 1) {
-        B_Level = B_Level - 1
+        B_Level = B_Level - 20
         basic.showNumber(B_Level)
         basic.pause(10)
     }
-    if (B_Level == 0 && Max_Bright == 1) {
+    if (B_Level <= 1 && Max_Bright == 1) {
         Max_Bright = 0
     }
     led.setBrightness(B_Level)
